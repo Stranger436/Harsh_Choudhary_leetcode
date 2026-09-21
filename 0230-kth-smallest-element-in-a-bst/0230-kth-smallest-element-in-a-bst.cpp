1fbfdbe506bb86923c366bb29e7a72417930096b
Harsh Choudhary <its.harsh.436@gmail.com>
2026-09-21 17:13:35 +0530

    Time: 0 ms (100%), Space: 24.4 MB (69.74%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -9,25 +9,66 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     void dfs(TreeNode* root, int k, int &cnt, int &ans) {
+//         if(root == NULL) return;
+//         // Left
+//         dfs(root->left, k, cnt, ans);
+//         // Node
+//         cnt++;
+//         if(cnt == k) {
+//             ans = root->val;
+//             return;
+//         }
+//         // Right
+//         dfs(root->right, k, cnt, ans);
+//     }
+//     int kthSmallest(TreeNode* root, int k) {
+//         int cnt = 0;
+//         int ans = -1;
+//         dfs(root, k, cnt, ans);
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-    void dfs(TreeNode* root, int k, int &cnt, int &ans) {
-        if(root == NULL) return;
-        // Left
-        dfs(root->left, k, cnt, ans);
-        // Node
-        cnt++;
-        if(cnt == k) {
-            ans = root->val;
-            return;
-        }
-        // Right
-        dfs(root->right, k, cnt, ans);
-    }
     int kthSmallest(TreeNode* root, int k) {
-        int cnt = 0;
+        TreeNode* cur = root;
         int ans = -1;
-        dfs(root, k, cnt, ans);
+        while(cur != NULL) {
+            // No left subtree
+            if(cur->left == NULL) {
+                k--;
+                if(k == 0){
+                    ans = cur->val;
+                }
+                cur = cur->right;
+            }
+            // Left subtree exists
+            else {
+                TreeNode* prev = cur->left;
+                // Find inorder predecessor
+                while(prev->right != NULL && prev->right != cur) {
+                    prev = prev->right;
+                }
+                // Create thread
+                if(prev->right == NULL) {
+                    prev->right = cur;
+                    cur = cur->left;
+                }
+                // Remove thread (when again visited)
+                else {
+                    prev->right = NULL;
+                    k--;
+                    if(k == 0){
+                        ans = cur->val;
+                    }
+                    cur = cur->right;
+                }
+            }
+        }
         return ans;
     }
 };
